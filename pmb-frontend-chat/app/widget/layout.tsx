ce38c0f037688f07cf0fcaf5fd6a0a3dabe620af
Merry <merryfebyanti@gmail.com>
2026-06-10 13:51:56 +0700

    remove bg black for widget

--- pmb-frontend-chat/app/widget/layout.tsx
+++ pmb-frontend-chat/app/widget/layout.tsx
@@ -12,7 +12,7 @@ export default function WidgetLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="h-screen w-full bg-black">
+    <div className="h-screen w-full ">
       <div className="h-full w-full max-w-[420px] mx-auto flex flex-col shadow-2xl border border-gray-800 rounded-3xl overflow-hidden bg-gray-950">
         {children}
       </div>
